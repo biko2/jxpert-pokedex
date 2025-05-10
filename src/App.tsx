55c1518e08823c64d4d93dfc8e7ea02e53359cba
unknown <upnalab@eim-por-71425.dyc.a.unavarra.es>
2025-05-10 13:07:22 +0200

    prueba error

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,6 @@ import rock from "./assets/rock.svg";
 import steel from "./assets/steel.svg";
 import water from "./assets/water.svg";
 import pokeball from "./assets/pokeball.svg";
-const muyVariable3 = "";
 
 /**
  *  Iconos de los tipos de Pokémon

1cef4e8fba664afc35ebbcd2e36800f03c94199d
Josu Alzueta <josu.alzueta@biko2.com>
2025-06-07 11:34:14 +0200

    Refactor: Add Header and Footer components

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import pokeball from "../assets/pokeball.svg";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <img src={pokeball} alt="" className="header__logo" />
+      <p className="header__title">Pokédex</p>
+    </header>
+  );
+};
